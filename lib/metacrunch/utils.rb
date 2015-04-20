2c6797436801de686ac3b90fea7220d5a0014bd6
René Sprotte <rene.sprotte@provideal.net>
2015-04-20 15:26:11 +0200

    Require relative.

--- lib/metacrunch/utils.rb
+++ lib/metacrunch/utils.rb
@@ -1,7 +1,6 @@
 module Metacrunch
   module Utils
+    require_relative "./utils/file_reader"
+    require_relative "./utils/file_reader_result"
   end
 end
-
-require_relative "./utils/file_reader"
-require_relative "./utils/file_reader_result"
