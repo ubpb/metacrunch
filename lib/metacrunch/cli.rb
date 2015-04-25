b4c23cddafde3517ba98b1af002aa762728bc864
René Sprotte <rene.sprotte@provideal.net>
2015-04-25 19:20:47 +0200

    Every metacrunch CLI extension should inherit from Metacrunch::Cli::Base and run commands using run_command helper.

--- lib/metacrunch/cli.rb
+++ lib/metacrunch/cli.rb
@@ -1,4 +1,14 @@
 module Metacrunch
   class Cli < Thor
+    class Base < Thor
+      private
+
+      def run_command(command, command_params = {})
+        clazz = command.class == Class ? command : command.to_s.constantize
+        raise ArgumentError, "command must be a Metacrunch::Command class" unless clazz < Metacrunch::Command
+
+        clazz.new(shell: shell, options: options).call(command_params)
+      end
+    end
   end
 end
