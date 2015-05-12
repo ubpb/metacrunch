dcc9ee3138b366a5a8493dcc56e1edd99c4181a2
René Sprotte <rene.sprotte@provideal.net>
2015-05-12 15:22:15 +0200

    Add a console command for playing around with the metacrunch API.

--- lib/metacrunch/cli/main.rb
+++ lib/metacrunch/cli/main.rb
@@ -1,7 +1,16 @@
 module Metacrunch
   class Cli
     class Main < Thor
-      # Define metacrunch root commands here
+      desc "console", "Start a console. It uses Pry if installed, IRB otherwise."
+      def console
+        begin
+          require "pry"
+          Pry.start
+        rescue LoadError
+          require "irb"
+          IRB.start
+        end
+      end
     end
   end
 end
