1e1af5d82e3abc16a6768337dc6670ac86ae02da
René Sprotte <rene.sprotte@provideal.net>
2015-05-20 13:38:34 +0200

    Bit refactoring.

--- lib/metacrunch/file_writer.rb
+++ lib/metacrunch/file_writer.rb
@@ -5,32 +5,35 @@ module Metacrunch
 
 
     def initialize(filename, override: false, compress: nil)
-      @path = File.expand_path(filename)
-
-      compress ||= @path.ends_with?(".gz")
+      @path       = File.expand_path(filename)
+      @compressed = (compress ||= @path.ends_with?(".gz"))
 
       if File.exist?(@path) && !override
         raise FileExistError, "File #{@path} already exists. Set override = true to override the existing file."
       end
-
-      @io = (compress == true) ? Zlib::GzipWriter.open(@path) : File.open(@path, "w")
     end
 
     def write(content = "", &block)
       if block_given?
-        yield(@io)
+        yield(io)
       else
-        [*content].map{|_content| @io.write(_content)}.sum
+        [*content].map{|_content| io.write(_content)}.sum
       end
     end
 
     def flush
-      @io.flush if @io
+      io.flush if io
     end
 
     def close
       flush
-      @io.close if @io
+      io.close if io
+    end
+
+  private
+
+    def io
+      @io ||= (@compressed == true) ? Zlib::GzipWriter.open(@path) : File.open(@path, "w")
     end
 
   end
