eb90b9b59ef4685e42766c40064cf3216ea54dd5
René Sprotte <rene.sprotte@provideal.net>
2015-05-28 11:46:10 +0200

    Add helper to the transformer.

--- lib/metacrunch/transformer.rb
+++ lib/metacrunch/transformer.rb
@@ -1,8 +1,10 @@
 module Metacrunch
   class Transformer
     require_relative "./transformer/step"
+    require_relative "./transformer/helper"
 
     attr_reader :source, :target, :options
+
 
     def initialize(source:, target:, options: {})
       @source  = source
@@ -17,6 +19,14 @@ module Metacrunch
         raise ArgumentError, "You need to provide a STEP or a block" if step_class.nil?
         step_class.new(self).perform
       end
+    end
+
+    def helper
+      @helper ||= Helper.new(self)
+    end
+
+    def register_helper(helper_module)
+      helper.class.send(:include, helper_module) # TODO: Benchmark this
     end
 
   end
--- lib/metacrunch/transformer/helper.rb
+++ lib/metacrunch/transformer/helper.rb
@@ -2,14 +2,10 @@ require_relative "../transformer"
 
 module Metacrunch
   class Transformer
-    class Step
+    class Helper
 
       def initialize(transformer)
         @transformer = transformer
-      end
-
-      def perform
-        raise NotImplementedError, "You must implement .perform() in your rule sub-class"
       end
 
       def transformer
@@ -26,10 +22,6 @@ module Metacrunch
 
       def options
         transformer.options
-      end
-
-      def helper
-        transformer.helper
       end
 
     end
