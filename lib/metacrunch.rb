5b207f15852ace8cc22f7dff428c64182495594f
René Sprotte <rene.sprotte@provideal.net>
2016-03-23 15:01:43 +0100

    Move existing stuff out of the way for some clean experiments.

--- lib/metacrunch.rb
+++ lib/metacrunch.rb
@@ -8,18 +8,6 @@ require "ox"
 
 module Metacrunch
   require_relative "./metacrunch/version"
-  require_relative "./metacrunch/cli"
-  require_relative "./metacrunch/command"
-  require_relative "./metacrunch/file"
-  require_relative "./metacrunch/file_reader"
-  require_relative "./metacrunch/file_reader_entry"
-  require_relative "./metacrunch/file_writer"
-  require_relative "./metacrunch/hash"
-  require_relative "./metacrunch/parallel"
-  require_relative "./metacrunch/processor"
-  require_relative "./metacrunch/tar_writer"
-  require_relative "./metacrunch/transformator"
-  require_relative "./metacrunch/transformer"
 
   def self.load_plugins
     Gem.find_latest_files("metacrunch_plugin.rb").each do |path|
