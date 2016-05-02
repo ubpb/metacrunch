d62cf1f1a3ba9e3309014edd003cb1d2fc6eab7a
René Sprotte <rene.sprotte@provideal.net>
2016-05-02 14:12:01 +0200

    Add `require` option.

--- lib/metacrunch/job/dsl/option_support.rb
+++ lib/metacrunch/job/dsl/option_support.rb
@@ -2,32 +2,76 @@ module Metacrunch
   class Job::Dsl::OptionSupport
 
     def register_options(args, &block)
-      registry = OptionProxy.new
+      options = {}
       registry.instance_eval(&block)
 
-      options = {}
+      registry.each do |key, opt_def|
+        # Set default value
+        options[key] = opt_def[:default]
 
-      OptionParser.new do |parser|
-        parser.banner = "Job specific options:"
-        registry.each do |key, opt_def|
-          options[key] = opt_def[:default]
-          if opt_def[:args].present?
-            parser.on(*opt_def[:args]) do |value|
-              options[key] = value
-            end
-          end
+        # Register with OptionParser
+        if opt_def[:args].present?
+          option = parser.define(*opt_def[:args]) { |value| options[key] = value }
+
+          option.desc << "REQUIRED" if opt_def[:required]
+          option.desc << "DEFAULT: #{opt_def[:default]}" if opt_def[:default].present?
+
+          parser_options[key] = option
         end
-      end.parse!(args || [])
+      end
+
+      # Finally parse CLI args with OptionParser
+      parser.parse!(args || [])
+
+      # Make sure required options are present
+      ensure_required_options!(options)
 
       options
     end
 
-    class OptionProxy
+  private
 
-      def add(name, *args, default:)
+    def parser
+      @parser ||= OptionParser.new do |parser|
+        parser.banner = "Usage: metacrunch run [options] JOB_FILE @@ [job-options] [ARGS]\nJob options:"
+      end
+    end
+
+    def parser_options
+      @parser_options ||= {}
+    end
+
+    def registry
+      @registry ||= OptionRegistry.new
+    end
+
+    def ensure_required_options!(options)
+      registry.each do |key, opt_def|
+        if opt_def[:required] && options[key].blank?
+          long_option = parser_options[key].long.try(:[], 0)
+          short_option = parser_options[key].short.try(:[], 0)
+
+          puts "Error: Required job option `#{long_option || short_option}` missing."
+          puts parser.help
+
+          exit(1)
+        end
+      end
+    end
+
+  private
+
+    class OptionRegistry
+
+      def add(name, *args, default: nil, required: false)
+        if default && required
+          raise ArgumentError, "You can't use `default` and `required` option at the same time."
+        end
+
         options[name.to_sym] = {
           args: args,
-          default: default
+          default: default,
+          required: required
         }
       end
 
