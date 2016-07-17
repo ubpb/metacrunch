7578a295931c48243f389b356fc9f39d73b2077f
René Sprotte <rene.sprotte@provideal.net>
2016-07-17 13:47:20 +0200

    Add an option to use upsert instead of insert.

--- lib/metacrunch/db/writer.rb
+++ lib/metacrunch/db/writer.rb
@@ -2,6 +2,9 @@ module Metacrunch
   class Db::Writer
 
     def initialize(database_connection_or_url, dataset_proc, options = {})
+      @use_upsert = options.delete(:use_upsert) || false
+      @id_key     = options.delete(:id_key)     || :id
+
       @db = if database_connection_or_url.is_a?(String)
         Sequel.connect(database_connection_or_url, options)
       else
@@ -14,17 +17,34 @@ module Metacrunch
     def write(data)
       if data.is_a?(Array)
         @db.transaction do
-          data.each do |d|
-            @dataset.insert(d) if d
-          end
+          data.each{|d| insert_or_upsert(d) }
         end
       else
-        @dataset.insert(data) if data
+        insert_or_upsert(data)
       end
     end
 
     def close
       @db.disconnect
+    end
+
+  private
+
+    def insert_or_upsert(data)
+      @use_upsert ? upsert(data) : insert(data)
+    end
+
+    def insert(data)
+      @dataset.insert(data) if data
+    end
+
+    def upsert(data)
+      if data
+        rec = @dataset.where(id: data[@id_key])
+        if 1 != rec.update(data)
+          insert(data)
+        end
+      end
     end
 
   end
