c4f57d9cdc21bac36dc1110eab3507467eff9509
René Sprotte <rene.sprotte@provideal.net>
2016-07-20 16:39:54 +0200

    Missing refactor step.

--- lib/metacrunch.rb
+++ lib/metacrunch.rb
@@ -7,7 +7,7 @@ module Metacrunch
   require_relative "metacrunch/version"
   require_relative "metacrunch/cli"
   require_relative "metacrunch/job"
-  require_relative "metacrunch/forking_source"
+  require_relative "metacrunch/parallel_processable_reader"
   require_relative "metacrunch/fs"
   require_relative "metacrunch/db"
   require_relative "metacrunch/redis"
