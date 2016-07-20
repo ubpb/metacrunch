329a70e67bf031ec6a35f206cc632b04f7331529
René Sprotte <rene.sprotte@provideal.net>
2016-07-20 16:51:46 +0200

    Metacrunch::Db::Reader now supports parallel processing.

--- lib/metacrunch/db/reader.rb
+++ lib/metacrunch/db/reader.rb
@@ -1,5 +1,8 @@
+require "metacrunch/db"
+
 module Metacrunch
   class Db::Reader
+    include Metacrunch::ParallelProcessableReader
 
     def initialize(database_connection_or_url, dataset_proc, options = {})
       @rows_per_fetch = options.delete(:rows_per_fetch) || 1000
@@ -10,14 +13,29 @@ module Metacrunch
         database_connection_or_url
       end
 
-      @dataset = dataset_proc.call(@db)
+      @dataset = dataset_proc.call(@db).unlimited
+      @total_numbers_of_records = @dataset.count
+
+      unless @dataset.opts[:order]
+        raise ArgumentError, "Metacrunch::Db::Reader requires the dataset be ordered."
+      end
     end
 
     def each(&block)
       return enum_for(__method__) unless block_given?
 
-      @dataset.paged_each(rows_per_fetch: @rows_per_fetch) do |row|
-        yield(row)
+      @db.transaction do
+        offset = (-number_of_processes * @rows_per_fetch) + (process_index * @rows_per_fetch)
+
+        loop do
+          offset = offset + (number_of_processes * @rows_per_fetch)
+
+          @dataset.limit(@rows_per_fetch).offset(offset).each do |row|
+            yield(row)
+          end
+
+          break if offset + @rows_per_fetch >= @total_numbers_of_records
+        end
       end
 
       self
