300747b8e8905f03ad20c19b24ea045c8eebf31a
René Sprotte <renspr@gmail.com>
2017-10-10 09:39:57 +0200

    Fix constant lookup.

--- lib/metacrunch/cli.rb
+++ lib/metacrunch/cli.rb
@@ -1,72 +1,70 @@
 require "optparse"
 
-module Metacrunch
-  class Cli
+class Metacrunch::Cli
 
-    def run
-      # Parse global options on order
-      job_argv = global_parser.order(ARGV)
-      # The first of the unparsed arguments is by definition the filename
-      # of the job.
-      job_file = job_argv[0]
-      # Manipulate ARGV so that the option handling for the job can work
-      ARGV.clear
-      job_argv[1..-1]&.each {|arg| ARGV << arg}
-      # Delete the old separator symbol for backward compatability
-      ARGV.delete_if{|arg| arg == "@@"}
-      # Finally run the job
-      run!(job_file)
-    rescue OptionParser::ParseError => e
-      error(e.message)
-    end
-
-  private
-
-    def global_parser
-      @global_parser ||= OptionParser.new do |opts|
-        opts.banner = <<-BANNER.strip_heredoc
-          #{ColorizedString["Usage:"].bold}
-
-            metacrunch [options] JOB_FILE [job-options] [ARGS...]
-
-          #{ColorizedString["Options:"].bold}
-        BANNER
-
-        opts.on("-v", "--version", "Show metacrunch version and exit") do
-          show_version
-        end
-      end
-    end
-
-    def show_version
-      puts Metacrunch::VERSION
-      exit(0)
-    end
-
-    def error(message)
-      puts ColorizedString["Error: #{message}\n"].red.bold
-      puts global_parser.help
-      exit(0)
-    end
-
-    def run!(job_file)
-      if job_file.blank?
-        error "You need to provide a job file."
-      elsif !File.exists?(job_file)
-        error "The file `#{job_file}` doesn't exist."
-      else
-        job_filename = File.expand_path(job_file)
-        dir = File.dirname(job_filename)
-
-        Dir.chdir(dir) do
-          run_job!(job_filename)
-        end
-      end
-    end
-
-    def run_job!(job_filename)
-      Metacrunch::Job.define(File.read(job_filename)).run
-    end
-
+  def run
+    # Parse global options on order
+    job_argv = global_parser.order(ARGV)
+    # The first of the unparsed arguments is by definition the filename
+    # of the job.
+    job_file = job_argv[0]
+    # Manipulate ARGV so that the option handling for the job can work
+    ARGV.clear
+    job_argv[1..-1]&.each {|arg| ARGV << arg}
+    # Delete the old separator symbol for backward compatability
+    ARGV.delete_if{|arg| arg == "@@"}
+    # Finally run the job
+    run!(job_file)
+  rescue OptionParser::ParseError => e
+    error(e.message)
   end
+
+private
+
+  def global_parser
+    @global_parser ||= OptionParser.new do |opts|
+      opts.banner = <<-BANNER.strip_heredoc
+        #{ColorizedString["Usage:"].bold}
+
+          metacrunch [options] JOB_FILE [job-options] [ARGS...]
+
+        #{ColorizedString["Options:"].bold}
+      BANNER
+
+      opts.on("-v", "--version", "Show metacrunch version and exit") do
+        show_version
+      end
+    end
+  end
+
+  def show_version
+    puts Metacrunch::VERSION
+    exit(0)
+  end
+
+  def error(message)
+    puts ColorizedString["Error: #{message}\n"].red.bold
+    puts global_parser.help
+    exit(0)
+  end
+
+  def run!(job_file)
+    if job_file.blank?
+      error "You need to provide a job file."
+    elsif !File.exists?(job_file)
+      error "The file `#{job_file}` doesn't exist."
+    else
+      job_filename = File.expand_path(job_file)
+      dir = File.dirname(job_filename)
+
+      Dir.chdir(dir) do
+        run_job!(job_filename)
+      end
+    end
+  end
+
+  def run_job!(job_filename)
+    Metacrunch::Job.define(File.read(job_filename)).run
+  end
+
 end
--- lib/metacrunch/job.rb
+++ lib/metacrunch/job.rb
@@ -1,151 +1,149 @@
-module Metacrunch
-  class Job
-    require_relative "job/dsl"
-    require_relative "job/buffer"
+class Metacrunch::Job # http://valve.github.io/blog/2013/10/26/constant-resolution-in-ruby/
+  require_relative "job/dsl"
+  require_relative "job/buffer"
 
-    attr_reader :dsl
+  attr_reader :dsl
 
-    class << self
-      def define(file_content = nil, &block)
-        self.new(file_content, &block)
-      end
+  class << self
+    def define(file_content = nil, &block)
+      self.new(file_content, &block)
     end
-
-    def initialize(file_content = nil, &block)
-      @dsl = Dsl.new(self)
-
-      @deprecator = ActiveSupport::Deprecation.new("5.0.0", "metacrunch")
-
-      if file_content
-        @dsl.instance_eval(file_content, "Check your metacrunch Job at Line")
-      elsif block_given?
-        @dsl.instance_eval(&block)
-      end
-    end
-
-    def source
-      @source
-    end
-
-    def source=(source)
-      ensure_source!(source)
-      @source = source
-    end
-
-    def destination
-      @destination
-    end
-
-    def destination=(destination)
-      ensure_destination!(destination)
-      @destination = destination
-    end
-
-    def pre_process
-      @pre_process
-    end
-
-    def pre_process=(callable)
-      ensure_callable!(callable)
-      @pre_process = callable
-    end
-
-    def post_process
-      @post_process
-    end
-
-    def post_process=(callable)
-      ensure_callable!(callable)
-      @post_process = callable
-    end
-
-    def transformations
-      @transformations ||= []
-    end
-
-    def add_transformation(callable, buffer_size: nil, buffer: nil)
-      ensure_callable!(callable)
-
-      if buffer_size && buffer_size.is_a?(Numeric)
-        @deprecator.deprecation_warning(:buffer_size, :buffer)
-        buffer = buffer_size
-      end
-
-      if buffer
-        transformations << Metacrunch::Job::Buffer.new(buffer)
-      end
-
-      transformations << callable
-    end
-
-    def run
-      run_pre_process
-
-      if source
-        # Run transformation for each data object available in source
-        source.each do |data|
-          data = run_transformations(data)
-          write_destination(data)
-        end
-
-        # Run all transformations a last time to flush existing buffers
-        data = run_transformations(nil, flush_buffers: true)
-        write_destination(data)
-
-        # Close destination
-        destination.close if destination
-      end
-
-      run_post_process
-
-      self
-    end
-
-  private
-
-    def ensure_source!(object)
-      raise ArgumentError, "#{object} doesn't respond to #each." unless object.respond_to?(:each)
-    end
-
-    def ensure_destination!(object)
-      raise ArgumentError, "#{object} doesn't respond to #write." unless object.respond_to?(:write)
-      raise ArgumentError, "#{object} doesn't respond to #close." unless object.respond_to?(:close)
-    end
-
-    def ensure_callable!(object)
-      raise ArgumentError, "#{object} doesn't respond to #call." unless object.respond_to?(:call)
-    end
-
-    def run_pre_process
-      pre_process.call if pre_process
-    end
-
-    def run_post_process
-      post_process.call if post_process
-    end
-
-    def run_transformations(data, flush_buffers: false)
-      transformations.each do |transformation|
-        if transformation.is_a?(Buffer)
-          buffer = transformation
-
-          if data
-            data = buffer.buffer(data)
-            data = buffer.flush if flush_buffers
-          else
-            data = buffer.flush
-          end
-        else
-          data = transformation.call(data) if data
-        end
-      end
-
-      data
-    end
-
-    def write_destination(data)
-      destination.write(data) if destination && data
-    end
-
   end
+
+  def initialize(file_content = nil, &block)
+    @dsl = Dsl.new(self)
+
+    @deprecator = ActiveSupport::Deprecation.new("5.0.0", "metacrunch")
+
+    if file_content
+      @dsl.instance_eval(file_content, "Check your metacrunch Job at Line")
+    elsif block_given?
+      @dsl.instance_eval(&block)
+    end
+  end
+
+  def source
+    @source
+  end
+
+  def source=(source)
+    ensure_source!(source)
+    @source = source
+  end
+
+  def destination
+    @destination
+  end
+
+  def destination=(destination)
+    ensure_destination!(destination)
+    @destination = destination
+  end
+
+  def pre_process
+    @pre_process
+  end
+
+  def pre_process=(callable)
+    ensure_callable!(callable)
+    @pre_process = callable
+  end
+
+  def post_process
+    @post_process
+  end
+
+  def post_process=(callable)
+    ensure_callable!(callable)
+    @post_process = callable
+  end
+
+  def transformations
+    @transformations ||= []
+  end
+
+  def add_transformation(callable, buffer_size: nil, buffer: nil)
+    ensure_callable!(callable)
+
+    if buffer_size && buffer_size.is_a?(Numeric)
+      @deprecator.deprecation_warning(:buffer_size, :buffer)
+      buffer = buffer_size
+    end
+
+    if buffer
+      transformations << Metacrunch::Job::Buffer.new(buffer)
+    end
+
+    transformations << callable
+  end
+
+  def run
+    run_pre_process
+
+    if source
+      # Run transformation for each data object available in source
+      source.each do |data|
+        data = run_transformations(data)
+        write_destination(data)
+      end
+
+      # Run all transformations a last time to flush existing buffers
+      data = run_transformations(nil, flush_buffers: true)
+      write_destination(data)
+
+      # Close destination
+      destination.close if destination
+    end
+
+    run_post_process
+
+    self
+  end
+
+private
+
+  def ensure_source!(object)
+    raise ArgumentError, "#{object} doesn't respond to #each." unless object.respond_to?(:each)
+  end
+
+  def ensure_destination!(object)
+    raise ArgumentError, "#{object} doesn't respond to #write." unless object.respond_to?(:write)
+    raise ArgumentError, "#{object} doesn't respond to #close." unless object.respond_to?(:close)
+  end
+
+  def ensure_callable!(object)
+    raise ArgumentError, "#{object} doesn't respond to #call." unless object.respond_to?(:call)
+  end
+
+  def run_pre_process
+    pre_process.call if pre_process
+  end
+
+  def run_post_process
+    post_process.call if post_process
+  end
+
+  def run_transformations(data, flush_buffers: false)
+    transformations.each do |transformation|
+      if transformation.is_a?(Buffer)
+        buffer = transformation
+
+        if data
+          data = buffer.buffer(data)
+          data = buffer.flush if flush_buffers
+        else
+          data = buffer.flush
+        end
+      else
+        data = transformation.call(data) if data
+      end
+    end
+
+    data
+  end
+
+  def write_destination(data)
+    destination.write(data) if destination && data
+  end
+
 end
